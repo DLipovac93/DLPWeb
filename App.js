66069006489c8ded2be7b41a1bc8ce056837afb1
Damir Lipovac <LipovacD93@gmail.com>
2024-03-30 06:37:17 -0700

    added some imaging things

--- App.js
+++ App.js
@@ -1,3 +1,14 @@
+
 function alertButton() {
-    alert("Fuck You")
+    alert("img");
+}
+function showContent(src, width, height, alt) {
+    var img = document.createElement("something.jpg");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+
+    // This next line will just add it to the <body> tag
+    document.body.appendChild(img);
 }
